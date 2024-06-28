7875a84e99049377bdc99ca2f24dd0874dc6a37b
Byron Thomas <byron.thomas@gmail.com>
2024-06-28 16:07:57 +0100

    Switch inference server to use FastAPI

--- rag_studio/inference_webserver.py
+++ rag_studio/inference_webserver.py
@@ -3,16 +3,22 @@ import os
 import shutil
 from pathlib import Path
 import sys
-from flask import Flask, jsonify, request, redirect
-from flask_cors import CORS
+
+from fastapi import FastAPI, HTTPException
+
+
+# from flask_cors import CORS
 from rag_studio.ragstore import RagStore
 from rag_studio.hf_repo_storage import get_last_commit, download_from_repo
 import logging
 from llama_index.core import (
     Settings,
 )
+from rag_studio.openai.schema import ChatCompletionRequest, CompletionRequest
+
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.vllm import Vllm
+
 from llama_index.core.base.llms.types import ChatMessage
 from llama_index.core.schema import BaseComponent
 import secrets
@@ -37,59 +43,59 @@ def make_embed_model(model_name, cache_folder):
     )
 
 
-def from_env_or_config(env_var, config, default=None):
-    """Get the value from the environment variable or the config."""
-    return os.environ.get(env_var, config.get(env_var)) or default
+# def from_env_or_config(env_var, config, default=None):
+#     """Get the value from the environment variable or the config."""
+#     return os.environ.get(env_var, config.get(env_var)) or default
 
 
-def set_model_params_from_request(llm: Vllm, data):
-    """Set the model parameters from the request data."""
-    # temperature: float = 1.0,
-    # n: int = 1,
-    # presence_penalty: float = 0.0,
-    # frequency_penalty: float = 0.0,
-    # top_p: float = 1.0,
-    # stop: Optional[List[str]] = None,
-    # max_tokens ->    # max_new_tokens: int = 512,
-    # logprobs: Optional[int] = None,
-    logger.info(
-        "Setting model params from request data: %s",
-        {k: v for k, v in data.items() if k not in {"messages", "prompt"}},
-    )
-    if "temperature" in data:
-        llm.temperature = data["temperature"]
-    if "n" in data and data["n"] != 1:
-        logger.error("Currently returning n > 1 completions is unsupported")
-        # llm.n = data["n"]
-        return "Currently returning n > 1 completions is unsupported"
-    if "presence_penalty" in data:
-        llm.presence_penalty = data["presence_penalty"]
-    if "frequency_penalty" in data:
-        llm.frequency_penalty = data["frequency_penalty"]
-    if "top_p" in data:
-        llm.top_p = data["top_p"]
-    if "stop" in data:
-        llm.stop = data["stop"]
-    if "max_tokens" in data:
-        llm.max_new_tokens = data["max_tokens"]
-    if "logprobs" in data:
-        # llm.logprobs = data["logprobs"]
-        logger.error("Currently logprobs output is unsupported")
-        return "Currently logprobs output is unsupported"
-    if (
-        "tools" in data
-        or "tool_choice" in data
-        or "functions" in data
-        or "function_call" in data
-    ):
-        logger.error("Currently use of tools / functions is unsupported")
-        return "Currently use of tools / functions is unsupported"
-    # Additionally the old completions API supports:
-    # best_of: Optional[int] = None,
-    if "best_of" in data:
-        llm.best_of = data["best_of"]
+# def set_model_params_from_request(llm: Vllm, data):
+#     """Set the model parameters from the request data."""
+#     # temperature: float = 1.0,
+#     # n: int = 1,
+#     # presence_penalty: float = 0.0,
+#     # frequency_penalty: float = 0.0,
+#     # top_p: float = 1.0,
+#     # stop: Optional[List[str]] = None,
+#     # max_tokens ->    # max_new_tokens: int = 512,
+#     # logprobs: Optional[int] = None,
+#     logger.info(
+#         "Setting model params from request data: %s",
+#         {k: v for k, v in data.items() if k not in {"messages", "prompt"}},
+#     )
+#     if "temperature" in data:
+#         llm.temperature = data["temperature"]
+#     if "n" in data and data["n"] != 1:
+#         logger.error("Currently returning n > 1 completions is unsupported")
+#         # llm.n = data["n"]
+#         return "Currently returning n > 1 completions is unsupported"
+#     if "presence_penalty" in data:
+#         llm.presence_penalty = data["presence_penalty"]
+#     if "frequency_penalty" in data:
+#         llm.frequency_penalty = data["frequency_penalty"]
+#     if "top_p" in data:
+#         llm.top_p = data["top_p"]
+#     if "stop" in data:
+#         llm.stop = data["stop"]
+#     if "max_tokens" in data:
+#         llm.max_new_tokens = data["max_tokens"]
+#     if "logprobs" in data:
+#         # llm.logprobs = data["logprobs"]
+#         logger.error("Currently logprobs output is unsupported")
+#         return "Currently logprobs output is unsupported"
+#     if (
+#         "tools" in data
+#         or "tool_choice" in data
+#         or "functions" in data
+#         or "function_call" in data
+#     ):
+#         logger.error("Currently use of tools / functions is unsupported")
+#         return "Currently use of tools / functions is unsupported"
+#     # Additionally the old completions API supports:
+#     # best_of: Optional[int] = None,
+#     if "best_of" in data:
+#         llm.best_of = data["best_of"]
 
-    return None
+#     return None
 
 
 def skeleton_openai_chat_response(req_id, completion_response, model_name="rag-chat"):
@@ -192,99 +198,85 @@ def skeleton_openai_completion_response(
     }
 
 
-def create_app(config=None, _engine=None):
-    """Create the main Flask app with the given config."""
-    config = config or {}
-    app = Flask(__name__)
-    CORS(app)
+app = FastAPI()
 
-    # Capture the current time
-    startTime = datetime.now()
 
-    # Need to wire in the RAG storage initialisation here, based on path
-    # from config object
-    rag_storage_path = from_env_or_config(
-        "RAG_STORAGE_PATH", config, default="/tmp/rag_storage"
-    )
-    logger.info("RAG storage path: %s", rag_storage_path)
-    rag_repo_id = from_env_or_config("RAG_REPO_ID", config)
-    if rag_repo_id is None:
-        logger.error("RAG_REPO_ID is not set")
-        sys.exit(1)
+# Capture the current time
+startTime = datetime.now()
 
-    model_download_dir = from_env_or_config(
-        "MODEL_DOWNLOAD_DIR", config, default="/tmp/models"
-    )
-    logger.info("Model storage path: %s", model_download_dir)
-    # Need to download the repo into the storage path, then can just initialise the store
-    # from that path
-    if os.path.exists(rag_storage_path):
-        logger.info("RAG storage path exists, removing before download")
-        # Remove the directory tree at rag_storage_path, even if the dir is not empty
-        shutil.rmtree(rag_storage_path)
+# Need to wire in the RAG storage initialisation here, based on path
+# from config object
+rag_storage_path = os.environ.get("RAG_STORAGE_PATH", "/tmp/rag_storage")
+logger.info("RAG storage path: %s", rag_storage_path)
+rag_repo_id = os.environ.get("RAG_REPO_ID")
+if rag_repo_id is None:
+    logger.error("RAG_REPO_ID is not set")
+    sys.exit(1)
 
-    download_from_repo(rag_repo_id, rag_storage_path)
-    rag_storage = RagStore(
-        rag_storage_path,
-        embed_model=make_embed_model(
-            "BAAI/bge-large-en-v1.5", f"{model_download_dir}/.hf-cache"
-        ),
-    )
+model_download_dir = os.environ.get("MODEL_DOWNLOAD_DIR", "/tmp/models")
+logger.info("Model storage path: %s", model_download_dir)
+# Need to download the repo into the storage path, then can just initialise the store
+# from that path
+if os.path.exists(rag_storage_path):
+    logger.info("RAG storage path exists, removing before download")
+    # Remove the directory tree at rag_storage_path, even if the dir is not empty
+    shutil.rmtree(rag_storage_path)
 
-    @app.route("/healthcheck")
-    def healthcheck_api():
-        """Healthcheck API to check if the API is running."""
-        return jsonify(
-            {
-                "message": "Inference API is running!",
-                "start_time": startTime.strftime("%Y-%m-%d %H:%M:%S"),
-            }
+download_from_repo(rag_repo_id, rag_storage_path)
+rag_storage = RagStore(
+    rag_storage_path,
+    embed_model=make_embed_model(
+        "BAAI/bge-large-en-v1.5", f"{model_download_dir}/.hf-cache"
+    ),
+)
+
+
+@app.route("/healthcheck")
+def healthcheck_api():
+    """Healthcheck API to check if the API is running."""
+    return {
+        "message": "Inference API is running!",
+        "start_time": startTime.strftime("%Y-%m-%d %H:%M:%S"),
+    }
+
+
+MODEL_NAME = "mistralai/Mistral-7B-Instruct-v0.1"
+llm = make_llm(
+    MODEL_NAME,
+    f"{model_download_dir}/vllm-via-llama-models",
+)
+chat_engine = rag_storage.make_chat_engine(llm=llm)
+query_engine = rag_storage.make_query_engine(llm=llm)
+
+
+@app.post("/v1/chat/completions")
+def chat_completions(req: ChatCompletionRequest):
+    """API to get completions for a given prompt."""
+    req_id = secrets.token_hex(16)
+    logger.info("Request ID: %s", req_id)
+    messages = req.messages
+    new_message = messages[-1]
+    if new_message.get("role") != "user":
+        raise HTTPException(
+            status_code=400,
+            detail=f"last message should be from user, but role was: {new_message.get('role')}",
         )
+    history = [ChatMessage(**m) for m in messages[:-1]]
 
-    MODEL_NAME = "mistralai/Mistral-7B-Instruct-v0.1"
-    llm = make_llm(
-        MODEL_NAME,
-        f"{model_download_dir}/vllm-via-llama-models",
-    )
-    chat_engine = rag_storage.make_chat_engine(llm=llm)
-    query_engine = rag_storage.make_query_engine(llm=llm)
+    problem_str = req.set_model_params_from_request(llm)
+    if problem_str:
+        return HTTPException(status_code=400, detail=problem_str)
+    result = chat_engine.chat(messages[-1], chat_history=history)
+    return skeleton_openai_chat_response(req_id, result.response, MODEL_NAME)
 
-    @app.post("/v1/chat/completions")
-    def chat_completions():
-        """API to get completions for a given prompt."""
-        req_id = secrets.token_hex(16)
-        logger.info("Request ID: %s", req_id)
-        data = request.json
-        messages = data.get("messages")
-        if messages is None:
-            return jsonify({"error": "messages is required"}), 400
-        new_message = messages[-1]
-        if new_message.get("role") != "user":
-            return jsonify({"error": "last message should be from user"}), 400
-        history = [ChatMessage(**m) for m in messages[:-1]]
 
-        problem_str = set_model_params_from_request(llm, data)
-        if problem_str:
-            return jsonify({"error": problem_str}), 400
-        completions = chat_engine.chat(messages[-1], chat_history=history)
-        return jsonify(
-            skeleton_openai_chat_response(req_id, completions.response, MODEL_NAME)
-        )
-
-    @app.post("/v1/completions")
-    def completions():
-        """API to get completions for a given prompt."""
-        req_id = secrets.token_hex(16)
-        logger.info("Request ID: %s", req_id)
-        data = request.json
-        problem_str = set_model_params_from_request(llm, data)
-        if problem_str:
-            return jsonify({"error": problem_str}), 400
-        completions = query_engine.query(data["prompt"])
-        return jsonify(
-            skeleton_openai_completion_response(
-                req_id, completions.response, MODEL_NAME
-            )
-        )
-
-    return app
+@app.post("/v1/completions")
+def completions(req: CompletionRequest):
+    """API to get completions for a given prompt."""
+    req_id = secrets.token_hex(16)
+    logger.info("Request ID: %s", req_id)
+    problem_str = req.set_model_params_from_request(llm)
+    if problem_str:
+        return HTTPException(status_code=400, detail=problem_str)
+    result = query_engine.query(req.prompt)
+    return skeleton_openai_completion_response(req_id, result.response, MODEL_NAME)
---
+++ rag_studio/openai/schema.py
@@ -0,0 +1,132 @@
+from typing import Any, Dict, List, Literal, Optional, Union
+from pydantic import BaseModel, Field
+from openai.types.chat import ChatCompletionMessageParam
+
+from llama_index.llms.vllm import Vllm
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class ResponseFormat(BaseModel):
+    type: Literal["json_object", "text"]
+
+
+class StreamOptions(BaseModel):
+    include_usage: Optional[bool]
+
+
+class ChatCompletionNamedFunction(BaseModel):
+    name: str
+
+
+class ChatCompletionNamedToolChoiceParam(BaseModel):
+    function: ChatCompletionNamedFunction
+    type: Literal["function"] = "function"
+
+
+class FunctionDefinition(BaseModel):
+    name: str
+    description: Optional[str] = None
+    parameters: Optional[Dict[str, Any]] = None
+
+
+class ChatCompletionToolsParam(BaseModel):
+    type: Literal["function"] = "function"
+    function: FunctionDefinition
+
+
+class CommonRequestFields(BaseModel):
+    model: str
+    best_of: Optional[int] = None
+    frequency_penalty: Optional[float] = 0.0
+    logit_bias: Optional[Dict[str, float]] = None
+    logprobs: Optional[int] = None
+    max_tokens: Optional[int] = None
+    n: int = 1
+    presence_penalty: Optional[float] = 0.0
+    stop: Optional[Union[str, List[str]]] = Field(default_factory=list)
+    stream: Optional[bool] = False
+    stream_options: Optional[StreamOptions] = None
+    suffix: Optional[str] = None
+    temperature: Optional[float] = 0.7
+    top_p: Optional[float] = None
+    user: Optional[str] = None
+
+    response_format: Optional[ResponseFormat] = None
+
+    def set_model_params_from_request(self, llm: Vllm):
+        """Set the model parameters from the request data."""
+        # temperature: float = 1.0,
+        # n: int = 1,
+        # presence_penalty: float = 0.0,
+        # frequency_penalty: float = 0.0,
+        # top_p: float = 1.0,
+        # stop: Optional[List[str]] = None,
+        # max_tokens ->    # max_new_tokens: int = 512,
+        # logprobs: Optional[int] = None,
+        logger.info(
+            "Setting model params from request data: %s",
+            self.model_dump(exclude_unset=True, exclude={"messages", "prompt"}),
+        )
+        if self.model != "rag_model":
+            return "No ability to change model from rag_model - this has been baked into the API"
+        if self.temperature:
+            llm.temperature = self.temperature
+        if self.n and self.n != 1:
+            logger.error("Currently returning n > 1 completions is unsupported")
+            # llm.n = self.n
+            return "Currently returning n > 1 completions is unsupported"
+        if self.presence_penalty:
+            llm.presence_penalty = self.presence_penalty
+        if self.frequency_penalty:
+            llm.frequency_penalty = self.frequency_penalty
+        if self.top_p:
+            llm.top_p = self.top_p
+        if self.stop:
+            llm.stop = self.stop
+        if self.max_tokens:
+            llm.max_new_tokens = self.max_tokens
+        if self.logprobs:
+            # llm.logprobs = self.logprobs
+            logger.error("Currently logprobs output is unsupported")
+            return "Currently logprobs output is unsupported"
+        if self.response_format and self.response_format.type != "text":
+            logger.error("Only text response format is supported")
+            return "Only text response format is supported"
+        if self.logit_bias:
+            logger.error("Currently logit_bias is unsupported")
+            return "Currently logit_bias is unsupported"
+        if self.stream:
+            logger.error("Currently streaming response is unsupported")
+            return "Currently streaming response is unsupported"
+        # Additionally the old completions API supports:
+        # best_of: Optional[int] = None,
+        if self.best_of:
+            llm.best_of = self.best_of
+
+        return None
+
+
+class ChatCompletionRequest(CommonRequestFields):
+    messages: List[ChatCompletionMessageParam]
+    top_logprobs: Optional[int] = 0
+    tools: Optional[List[ChatCompletionToolsParam]] = None
+    tool_choice: Optional[
+        Union[Literal["none"], ChatCompletionNamedToolChoiceParam]
+    ] = "none"
+
+    def set_model_params_from_request(self, llm: Vllm):
+        if self.tools or self.tool_choice != "none":
+            logger.error("Currently use of tools / functions is unsupported")
+            return "Currently use of tools / functions is unsupported"
+        if self.top_logprobs:
+            logger.error("Currently logprobs output is unsupported")
+            return "Currently logprobs output is unsupported"
+        return super().set_model_params_from_request(llm)
+
+
+class CompletionRequest(CommonRequestFields):
+    prompt: Union[List[int], List[List[int]], str, List[str]]
+    echo: Optional[bool] = False
