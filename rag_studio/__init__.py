536b71ebb8e0c796638517a47c73c8742862d95a
Byron Thomas <byron.thomas@gmail.com>
2024-06-28 11:33:06 +0100

    De-dupe logging

--- rag_studio/__init__.py
+++ rag_studio/__init__.py
@@ -4,4 +4,3 @@ import sys
 
 lvl = logging.getLevelName(os.environ.get("LOG_LEVEL", "DEBUG"))
 logging.basicConfig(stream=sys.stdout, level=lvl)
-logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
