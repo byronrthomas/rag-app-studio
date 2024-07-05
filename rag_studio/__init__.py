ef99618aadf14836f0203afcefca353120527260
Byron Thomas <byron.thomas@gmail.com>
2024-07-05 16:50:46 +0100

    Logging is also done to filesystem, with log rotation

--- rag_studio/__init__.py
+++ rag_studio/__init__.py
@@ -1,6 +1,35 @@
 import logging
 import os
 import sys
+from logging.handlers import RotatingFileHandler
 
-lvl = logging.getLevelName(os.environ.get("LOG_LEVEL", "DEBUG"))
-logging.basicConfig(stream=sys.stdout, level=lvl)
+# Retrieve log level from environment or use "DEBUG" as default
+log_level_name = os.environ.get("LOG_LEVEL", "DEBUG")
+log_level = logging.getLevelName(log_level_name)
+
+# Get the root logger
+root_logger = logging.getLogger()
+root_logger.setLevel(log_level)
+
+# Create console handler for output to stdout
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setLevel(log_level)
+
+# Create rotating file handler
+# Adjust 'maxBytes' and 'backupCount' to your needs
+LOG_FILE_FOLDER = "/var/log/rag_studio"
+if not os.path.exists(LOG_FILE_FOLDER):
+    os.makedirs(LOG_FILE_FOLDER)
+file_handler = RotatingFileHandler(
+    LOG_FILE_FOLDER + "/application.log", maxBytes=1048576, backupCount=5
+)
+file_handler.setLevel(log_level)
+
+# Create formatter and add it to the handlers
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+# Add handlers to the root logger
+root_logger.addHandler(console_handler)
+root_logger.addHandler(file_handler)
